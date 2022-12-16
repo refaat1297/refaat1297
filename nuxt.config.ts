bf60695b942b4ce076ae96cbba526599d868085c
refaat1297 <a.refaat1297@gmail.com>
2022-12-16 17:52:31 +0200

    add main page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  devServer: {
+    port: 8080,
+    url: "http://localhost:8080"
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>hello this is main page</div>
+</template>
