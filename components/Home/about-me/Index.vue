41b847cd893d4377097a5e18039abb5bf01095bd
refaat1297 <a.refaat1297@gmail.com>
2022-12-28 14:32:07 +0200

    UI 🎨: add who I Am section

--- components/Home/about-me/Index.vue
+++ components/Home/about-me/Index.vue
@@ -2,15 +2,48 @@
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold">
-      Hello world!
-    </h1>
+  <div class="w-full">
+    <div class="who-iam h-screen relative">
+      <div class="container mx-auto h-full text-white grid justify-start items-center">
+        <HomeAboutMeLogoMask class="logo-mask absolute left-1/2 top-1/2 opacity-50" />
+        <div class="content select-none">
+          <h1 class="ar-black-font text-6xl md:text-7xl lg:text-8xl">Ahmed Refaat</h1>
+          <p class="text-3xl md:text-4xl ar-extra-light-font italic">Interactive Front-end Developer</p>
+          <a href="#" class="text-xl md:text-2xl">About Me</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 
-<style scoped>
+<style lang="scss" scoped>
+.who-iam {
+  background-color: #1c1d25;
 
+  .logo-mask {
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    animation: jump 5s infinite;
+  }
+
+  .content {
+    z-index: 2;
+  }
+}
+
+@keyframes jump {
+  0% {
+    transform: translate(-50%, -50%);
+  }
+
+  50% {
+    transform: translate(-50%, -40%);
+  }
+
+  100% {
+    transform: translate(-50%, -50%);
+  }
+}
 </style>
