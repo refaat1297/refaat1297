85cbd89884e0188ccc6020edd514b12319c4a2a0
refaat1297 <a.refaat1297@gmail.com>
2022-12-30 19:51:07 +0200

    Feat 🚀: add latest works section

---
+++ components/Home/latest-works/Index.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { projects } from "~/components/Home/latest-works/data";
+</script>
+
+<template>
+  <div class="latest-works w-full bg-[#1c1d25] py-20">
+    <div class="container">
+      <h2 class="ar-semi-bold-font text-4xl md:text-5xl lg:text-6xl text-center text-white mb-8">Latest Works</h2>
+
+      <div class="projects">
+        <HomeLatestWorksProject
+          v-for="(project, index) in projects"
+          :key="`project-${index}`"
+          :project="project"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.latest-works {
+  .projects {
+
+  }
+}
+</style>
+
